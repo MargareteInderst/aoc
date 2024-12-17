12e45f1ef5c61cd04ba89994b0dd9da5b3448b19
MargareteInderst <margarete.inderst@gmail.com>
2024-12-17 22:14:04 +0100

    finished

--- day04/aoc08.cc
+++ day04/aoc08.cc
@@ -36,38 +36,6 @@ int diagonal(std::vector<std::string>& input) {
   return sum;
 }
 
-int horizontal(std::vector<std::string>& input) {
-  int sum = 0;
-
-  // i rows
-  for (unsigned i = 1; i < input.size() - 1; ++i) {
-    // j columns
-    for (unsigned j = 0; j < input.at(0).size() - 2; ++j) {
-      if (input.at(i).at(j) == 'M' && input.at(i).at(j + 1) == 'A' &&
-          input.at(i).at(j + 2) == 'S') {
-        if (input.at(i - 1).at(j + 1) == 'M' &&
-            input.at(i + 1).at(j + 1) == 'S') {
-          ++sum;
-        } else if (input.at(i - 1).at(j + 1) == 'S' &&
-                   input.at(i + 1).at(j + 1) == 'M') {
-          ++sum;
-        }
-      } else if (input.at(i).at(j) == 'S' && input.at(i).at(j + 1) == 'A' &&
-                 input.at(i).at(j + 2) == 'M') {
-        if (input.at(i - 1).at(j + 1) == 'M' &&
-            input.at(i + 1).at(j + 1) == 'S') {
-          ++sum;
-        } else if (input.at(i - 1).at(j + 1) == 'S' &&
-                   input.at(i + 1).at(j + 1) == 'M') {
-          ++sum;
-        }
-      }
-    }
-  }
-
-  return sum;
-}
-
 int main() {
   std::vector<std::string> input;
   std::string line;
@@ -76,7 +44,7 @@ int main() {
     input.push_back(line);
   }
 
-  std::cout << horizontal(input) + diagonal(input) << std::endl;
+  std::cout << diagonal(input) << std::endl;
 
   return 0;
 }
