16ff741000822125cb0be9e2fc60bb11f05b94dc
MargareteInderst <margarete.inderst@gmail.com>
2024-12-17 23:25:36 +0100

    ' ' vergessen upsi

--- day03/aoc06.cc
+++ day03/aoc06.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 std::string active(std::string& input){
 
@@ -15,7 +16,7 @@ std::string active(std::string& input){
         input.erase(it,it+4);
         ix = ix-*it;
       } 
-      std::cerr << input << "\n";
+      //std::cerr << input << "\n";
       ix = input.find("don't()");
       it = input.begin() + ix;
       //std::cerr << *(it-1);
@@ -28,7 +29,7 @@ std::string active(std::string& input){
 
       input.erase(it, input.begin()+ix);
 
-      std::cerr << input << "\n";
+      //std::cerr << input << "\n";
 
       it = input.begin() + ix - *it;
 
@@ -49,47 +50,91 @@ int main() {
   int sum = 0;
 
   std::string input;
-
-  std::cin >> input;
-
-  std::string sequence = active(input);
-  //std::string sequence = input;
-
   
-  char li;
-  char lii;
-  char liii;
-  char ri;
-  char rii;
-  char riii;
+  while(std::getline(std::cin, input)){
 
-  std::string::iterator it = sequence.begin();
+    std::string sequence = active(input);
+    //std::string sequence = input;
 
-  while (it != sequence.end()) {
-    if (*it == 'm' && it + 1 != sequence.end()) {
-      ++it;
-      if (*it == 'u' && it + 1 != sequence.end()) {
+    
+    char li;
+    char lii;
+    char liii;
+    char ri;
+    char rii;
+    char riii;
+
+    std::string::iterator it = sequence.begin();
+
+    while (it != sequence.end()) {
+      if (*it == 'm' && it + 1 != sequence.end()) {
         ++it;
-
-        if (*it == 'l' && it + 1 != sequence.end()) {
+        if (*it == 'u' && it + 1 != sequence.end()) {
           ++it;
 
-          if (*it == '(' && it + 1 != sequence.end()) {
+          if (*it == 'l' && it + 1 != sequence.end()) {
             ++it;
 
-            if (*it >= '0' && *it <= '9' && it + 1 != sequence.end()) {
-              li = *it;
+            if (*it == '(' && it + 1 != sequence.end()) {
               ++it;
 
               if (*it >= '0' && *it <= '9' && it + 1 != sequence.end()) {
-                lii = *it;
+                li = *it;
                 ++it;
 
                 if (*it >= '0' && *it <= '9' && it + 1 != sequence.end()) {
-                  liii = *it;
+                  lii = *it;
                   ++it;
 
-                  if (*it == ',' && it + 1 != sequence.end()) {
+                  if (*it >= '0' && *it <= '9' && it + 1 != sequence.end()) {
+                    liii = *it;
+                    ++it;
+
+                    if (*it == ',' && it + 1 != sequence.end()) {
+                      ++it;
+
+                      if (*it >= '0' && *it <= '9' && it + 1 != sequence.end()) {
+                        ri = *it;
+                        ++it;
+
+                        if (*it >= '0' && *it <= '9' && it + 1 != sequence.end()) {
+                          rii = *it;
+                          ++it;
+
+                          if (*it >= '0' && *it <= '9' && it + 1 != sequence.end()) {
+                            riii = *it;
+                            ++it;
+
+                            // mul(abc,xyz)
+                            if (*it == ')') {
+                              int numl = (li - 48) * 100 + (lii - 48) * 10 + (liii - 48);
+                              int numr = (ri - 48) * 100 + (rii - 48) * 10 + (riii - 48);
+
+                              sum += (numl * numr);
+                            }
+
+                          }  // mul(abc,xy)
+                          else if (*it == ')') {
+                            
+                            int numl = (li - 48) * 100 + (lii - 48) * 10 + (liii - 48);
+                            int numr = (ri - 48) * 10 + (rii - 48);
+
+                            sum += (numl * numr);
+                          }
+
+                        }  // mul(abc,x)
+                        else if (*it == ')') {
+
+                          int numl = (li - 48) * 100 + (lii - 48) * 10 + (liii - 48);
+                          int numr = (ri - 48);
+
+                          sum += (numl * numr);
+                        }
+                      }
+                    }
+
+                  }  // mul(ab,...
+                  else if (*it == ',' && it + 1 != sequence.end()) {
                     ++it;
 
                     if (*it >= '0' && *it <= '9' && it + 1 != sequence.end()) {
@@ -104,27 +149,28 @@ int main() {
                           riii = *it;
                           ++it;
 
-                          // mul(abc,xyz)
+                          // mul(ab,xyz)
                           if (*it == ')') {
-                            int numl = (li - 48) * 100 + (lii - 48) * 10 + (liii - 48);
+                            
+                            int numl = (li - 48) * 10 + (lii - 48);
                             int numr = (ri - 48) * 100 + (rii - 48) * 10 + (riii - 48);
 
                             sum += (numl * numr);
                           }
 
-                        }  // mul(abc,xy)
+                        }  // mul(ab,xy)
                         else if (*it == ')') {
-                          
-                          int numl = (li - 48) * 100 + (lii - 48) * 10 + (liii - 48);
+
+                          int numl = (li - 48) * 10 + (lii - 48);
                           int numr = (ri - 48) * 10 + (rii - 48);
 
                           sum += (numl * numr);
                         }
 
-                      }  // mul(abc,x)
+                      }  // mul(ab,x)
                       else if (*it == ')') {
 
-                        int numl = (li - 48) * 100 + (lii - 48) * 10 + (liii - 48);
+                        int numl = (li - 48) * 10 + (lii - 48);
                         int numr = (ri - 48);
 
                         sum += (numl * numr);
@@ -132,7 +178,7 @@ int main() {
                     }
                   }
 
-                }  // mul(ab,...
+                }  // mul(a,...
                 else if (*it == ',' && it + 1 != sequence.end()) {
                   ++it;
 
@@ -148,74 +194,30 @@ int main() {
                         riii = *it;
                         ++it;
 
-                        // mul(ab,xyz)
+                        // mul(a,xyz)
                         if (*it == ')') {
-                          
-                          int numl = (li - 48) * 10 + (lii - 48);
+                          int numl = (li - 48);
                           int numr = (ri - 48) * 100 + (rii - 48) * 10 + (riii - 48);
 
                           sum += (numl * numr);
                         }
 
-                      }  // mul(ab,xy)
+                      }  // mul(a,xy)
                       else if (*it == ')') {
 
-                        int numl = (li - 48) * 10 + (lii - 48);
+                        int numl = (li - 48);
                         int numr = (ri - 48) * 10 + (rii - 48);
 
                         sum += (numl * numr);
                       }
-
-                    }  // mul(ab,x)
+                    }  // mul(a,x)
                     else if (*it == ')') {
 
-                      int numl = (li - 48) * 10 + (lii - 48);
+                      int numl = (li - 48);
                       int numr = (ri - 48);
 
                       sum += (numl * numr);
                     }
-                  }
-                }
-
-              }  // mul(a,...
-              else if (*it == ',' && it + 1 != sequence.end()) {
-                ++it;
-
-                if (*it >= '0' && *it <= '9' && it + 1 != sequence.end()) {
-                  ri = *it;
-                  ++it;
-
-                  if (*it >= '0' && *it <= '9' && it + 1 != sequence.end()) {
-                    rii = *it;
-                    ++it;
-
-                    if (*it >= '0' && *it <= '9' && it + 1 != sequence.end()) {
-                      riii = *it;
-                      ++it;
-
-                      // mul(a,xyz)
-                      if (*it == ')') {
-                        int numl = (li - 48);
-                        int numr = (ri - 48) * 100 + (rii - 48) * 10 + (riii - 48);
-
-                        sum += (numl * numr);
-                      }
-
-                    }  // mul(a,xy)
-                    else if (*it == ')') {
-
-                      int numl = (li - 48);
-                      int numr = (ri - 48) * 10 + (rii - 48);
-
-                      sum += (numl * numr);
-                    }
-                  }  // mul(a,x)
-                  else if (*it == ')') {
-
-                    int numl = (li - 48);
-                    int numr = (ri - 48);
-
-                    sum += (numl * numr);
                   }
                 }
               }
@@ -223,8 +225,8 @@ int main() {
           }
         }
       }
+      ++it;
     }
-    ++it;
   }
 
   //don't()mul(15,10)do()do()don't()mul(5,5)
