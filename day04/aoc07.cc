edbb2d693a5deb9dfc095c7a0d16dfd0f26534d1
MargareteInderst <margarete.inderst@gmail.com>
2024-12-17 21:50:57 +0100

    formatiert

--- day04/aoc07.cc
+++ day04/aoc07.cc
@@ -1,21 +1,20 @@
 #include <iostream>
-#include <vector>
 #include <sstream>
 #include <string>
+#include <vector>
 
-int horizontal(std::vector<std::string>& input){
-
+int horizontal(std::vector<std::string>& input) {
   int sum = 0;
 
-  //i goes through the rows
-  for(unsigned i = 0; i < input.size(); ++i){
-    //j goes through the columns
-    for(unsigned j = 0; j < input.at(0).size()-3; ++j){
-      
-      if(input.at(i).at(j) == 'X' && input.at(i).at(j+1) == 'M' && input.at(i).at(j+2) == 'A' && input.at(i).at(j+3) == 'S'){
+  // i goes through the rows
+  for (unsigned i = 0; i < input.size(); ++i) {
+    // j goes through the columns
+    for (unsigned j = 0; j < input.at(0).size() - 3; ++j) {
+      if (input.at(i).at(j) == 'X' && input.at(i).at(j + 1) == 'M' &&
+          input.at(i).at(j + 2) == 'A' && input.at(i).at(j + 3) == 'S') {
         ++sum;
-      }
-      else if(input.at(i).at(j) == 'S' && input.at(i).at(j+1) == 'A' && input.at(i).at(j+2) == 'M' && input.at(i).at(j+3) == 'X'){
+      } else if (input.at(i).at(j) == 'S' && input.at(i).at(j + 1) == 'A' &&
+                 input.at(i).at(j + 2) == 'M' && input.at(i).at(j + 3) == 'X') {
         ++sum;
       }
     }
@@ -24,19 +23,20 @@ int horizontal(std::vector<std::string>& input){
   return sum;
 }
 
-int diagonalright(std::vector<std::string>& input){
-
+int diagonalright(std::vector<std::string>& input) {
   int sum = 0;
 
-  //i goes through the rows
-  for(unsigned i = 0; i < input.size()-3; ++i){
-    //j goes through the columns
-    for(unsigned j = 0; j < input.at(i).size()-3; ++j){
-      
-      if(input.at(i).at(j) == 'X' && input.at(i+1).at(j+1) == 'M' && input.at(i+2).at(j+2) == 'A' && input.at(i+3).at(j+3) == 'S'){
+  // i goes through the rows
+  for (unsigned i = 0; i < input.size() - 3; ++i) {
+    // j goes through the columns
+    for (unsigned j = 0; j < input.at(i).size() - 3; ++j) {
+      if (input.at(i).at(j) == 'X' && input.at(i + 1).at(j + 1) == 'M' &&
+          input.at(i + 2).at(j + 2) == 'A' &&
+          input.at(i + 3).at(j + 3) == 'S') {
         ++sum;
-      }
-      else if(input.at(i).at(j) == 'S' && input.at(i+1).at(j+1) == 'A' && input.at(i+2).at(j+2) == 'M' && input.at(i+3).at(j+3) == 'X'){
+      } else if (input.at(i).at(j) == 'S' && input.at(i + 1).at(j + 1) == 'A' &&
+                 input.at(i + 2).at(j + 2) == 'M' &&
+                 input.at(i + 3).at(j + 3) == 'X') {
         ++sum;
       }
     }
@@ -45,19 +45,20 @@ int diagonalright(std::vector<std::string>& input){
   return sum;
 }
 
-int diagonalleft(std::vector<std::string>& input){
-
+int diagonalleft(std::vector<std::string>& input) {
   int sum = 0;
 
-  //i goes through the rows
-  for(unsigned i = 0; i < input.size() - 3; ++i){
-    //j goes through the columns
-    for(unsigned j = 3; j < input.at(i).size(); ++j){
-      
-      if(input.at(i).at(j) == 'X' && input.at(i+1).at(j-1) == 'M' && input.at(i+2).at(j-2) == 'A' && input.at(i+3).at(j-3) == 'S'){
+  // i goes through the rows
+  for (unsigned i = 0; i < input.size() - 3; ++i) {
+    // j goes through the columns
+    for (unsigned j = 3; j < input.at(i).size(); ++j) {
+      if (input.at(i).at(j) == 'X' && input.at(i + 1).at(j - 1) == 'M' &&
+          input.at(i + 2).at(j - 2) == 'A' &&
+          input.at(i + 3).at(j - 3) == 'S') {
         ++sum;
-      }
-      else if(input.at(i).at(j) == 'S' && input.at(i+1).at(j-1) == 'A' && input.at(i+2).at(j-2) == 'M' && input.at(i+3).at(j-3) == 'X'){
+      } else if (input.at(i).at(j) == 'S' && input.at(i + 1).at(j - 1) == 'A' &&
+                 input.at(i + 2).at(j - 2) == 'M' &&
+                 input.at(i + 3).at(j - 3) == 'X') {
         ++sum;
       }
     }
@@ -66,19 +67,18 @@ int diagonalleft(std::vector<std::string>& input){
   return sum;
 }
 
-int vertical(std::vector<std::string>& input){
-
+int vertical(std::vector<std::string>& input) {
   int sum = 0;
 
-  //i goes through the rows
-  for(unsigned i = 0; i < input.size()-3; ++i){
-    //j goes through the columns
-    for(unsigned j = 0; j < input.at(i).size(); ++j){
-      
-      if(input.at(i).at(j) == 'X' && input.at(i+1).at(j) == 'M' && input.at(i+2).at(j) == 'A' && input.at(i+3).at(j) == 'S'){
+  // i goes through the rows
+  for (unsigned i = 0; i < input.size() - 3; ++i) {
+    // j goes through the columns
+    for (unsigned j = 0; j < input.at(i).size(); ++j) {
+      if (input.at(i).at(j) == 'X' && input.at(i + 1).at(j) == 'M' &&
+          input.at(i + 2).at(j) == 'A' && input.at(i + 3).at(j) == 'S') {
         ++sum;
-      }
-      else if(input.at(i).at(j) == 'S' && input.at(i+1).at(j) == 'A' && input.at(i+2).at(j) == 'M' && input.at(i+3).at(j) == 'X'){
+      } else if (input.at(i).at(j) == 'S' && input.at(i + 1).at(j) == 'A' &&
+                 input.at(i + 2).at(j) == 'M' && input.at(i + 3).at(j) == 'X') {
         ++sum;
       }
     }
@@ -87,18 +87,17 @@ int vertical(std::vector<std::string>& input){
   return sum;
 }
 
-int main(){
-
+int main() {
   std::vector<std::string> input;
   std::string line;
 
-  while(std::getline(std::cin,line)){
-
+  while (std::getline(std::cin, line)) {
     input.push_back(line);
-
   }
 
-  std::cout << horizontal(input) + diagonalright(input) + diagonalleft(input) + vertical(input) << std::endl;
+  std::cout << horizontal(input) + diagonalright(input) + diagonalleft(input) +
+                   vertical(input)
+            << std::endl;
 
   return 0;
 }
